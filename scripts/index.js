eba643d367d3c4e393c56a7bdbf535fa13e37c49
Mugilarasan <95951189+mugilmalathi@users.noreply.github.com>
2022-06-24 19:45:55 +0530

    basic structure and json added

---
+++ scripts/index.js
@@ -0,0 +1,39 @@
+
+function latestNews(){
+    fetch("http://localhost:8080/getTimeStories",{
+        method: 'GET'
+    }).then((result)=>{
+        result.json().then((res)=>{
+            console.log(res);
+            getLatestNews(res)
+        })
+    })
+}
+
+latestNews()
+
+function getLatestNews(arr){
+
+    arr.map((e)=>{
+        let div = document.createElement('div');
+        div.setAttribute('id', 'latestnews')
+
+        let title = document.createElement('h4');
+        title.innerText = e.name;
+
+        let link1 = document.createElement('a');
+        link1.innerText = e.url;
+        link1.href = e.url;
+
+        let news = document.createElement('h4');
+        news.innerText = e.selection2;
+
+        let link2 = document.createElement('a');
+        link2.innerText = e.selection2_url;
+        link2.href = e.selection2_url;
+
+        div.append(title, link1, news, link2)
+        document.getElementById('right_div').append(div)
+    })
+
+}

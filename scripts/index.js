d5afcb814a68d9d16a7d6ae0e933b7756d9715da
Mugilarasan <95951189+mugilmalathi@users.noreply.github.com>
2022-06-24 20:10:35 +0530

    flow completed

--- scripts/index.js
+++ scripts/index.js
@@ -37,3 +37,39 @@ function getLatestNews(arr){
     })
 
 }
+
+function homeNews(){
+    fetch("http://localhost:8080/homenews",{
+        method: 'GET'
+    }).then((result)=>{
+        result.json().then((res)=>{
+            console.log(res);
+            gethomeNews(res)
+        })
+    })
+}
+
+homeNews()
+
+function gethomeNews(arr){
+
+    arr.map((e)=>{
+        let div = document.createElement('div');
+        div.setAttribute('id', 'latestnews')
+
+        let title = document.createElement('h4');
+        title.innerText = e.name;
+
+        let link1 = document.createElement('a');
+        link1.innerText = e.url;
+        link1.href = e.url;
+
+        let news = document.createElement('p');
+        news.innerText = e.selection2;
+
+
+        div.append(title, link1, news)
+        document.getElementById('left_div').append(div)
+    })
+
+}
